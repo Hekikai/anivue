3298bfbcee2d6db96abf3410b994303bd30b6132
Artem Kolesnikov <katanaosu@vk.com>
2023-07-29 22:19:16 +0300

    🔧 Add paddings to uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig, presetUno, presetWebFonts } from 'unocss'
-import { COLOR_PRESET } from './uno-presets/colors'
-import { TYPOGRAPHY_PRESET } from './uno-presets/typography'
+import { defineConfig, presetUno, presetWebFonts } from 'unocss';
+import { COLOR_PRESET } from './uno-presets/colors';
+import { TYPOGRAPHY_PRESET } from './uno-presets/typography';
 
 export default defineConfig({
-  rules: [],
+  rules: [
+    [/^p-(\d+)$/, match => ({ padding: `${match[1]}px` })],
+    [/^px-(\d+)$/, match => ({ 'padding-left': `${match[1]}px`, 'padding-right': `${match[1]}px` })],
+    [/^py-(\d+)$/, match => ({ 'padding-top': `${match[1]}px`, 'padding-bottom': `${match[1]}px` })],
+  ],
   presets: [
     COLOR_PRESET,
     TYPOGRAPHY_PRESET,
@@ -19,4 +23,4 @@ export default defineConfig({
     }),
   ],
   configDeps: ['./uno-presets/typography/index.ts', './uno-presets/colors/index.ts'],
-})
+});
