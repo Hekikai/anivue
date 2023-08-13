29f4ad4ca912ad461ccf3187d024cad8aa3f8a8b
Artem Kolesnikov <katanaosu@vk.com>
2023-08-13 21:06:14 +0300

    ♻️ Add primary and tertiary styles to AButton.vue

--- src/components/button/AButton.vue
+++ src/components/button/AButton.vue
@@ -1,25 +1,33 @@
 <script setup lang="ts">
 type AButttonType = 'primary' | 'secondary' | 'tertiary'
+type AButtonSize = 'md' | 'xs';
 
-defineProps<{
+withDefaults(defineProps<{
   type: AButttonType
-  text: string
-}>();
+  size?: AButtonSize
+  text?: string
+}>(), {
+  size: 'md',
+  text: '',
+});
 </script>
 
 <template>
   <button
     class="text-base-medium"
-    :class="[$style.button, $style[type]]"
+    :class="[$style.button, $style[type], $style[size], !text && $style.noText]"
   >
+    <slot name="left-icon" />
     {{ text }}
+    <slot name="right-icon" />
   </button>
 </template>
 
 <style module lang="scss">
 .button {
-  height: 52px;
-  padding: 14px 26px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
   color: var(--neutral-900);
   border-radius: 14px;
@@ -44,6 +52,44 @@ defineProps<{
     color: var(--neutral-400);
     background-color: var(--lime-200);
     cursor: not-allowed;
+  }
+}
+
+.tertiary {
+  background-color: var(--neutral-100);
+
+  &:hover {
+    background-color: var(--neutral-200);
+  }
+
+  &:active {
+    color: var(--neutral-700);
+    background-color: var(--neutral-200);
+  }
+
+  &:disabled {
+    color: var(--neutral-400);
+    background-color: var(--neutral-100);
+  }
+}
+
+.md {
+  height: 52px;
+  padding: 14px 26px;
+  gap: 8px;
+
+  &.noText {
+    padding: 14px 16px;
+  }
+}
+
+.xs {
+  height: 44px;
+  padding: 10px 18px;
+  gap: 6px;
+
+  &.noText {
+    padding: 10px;
   }
 }
 </style>
