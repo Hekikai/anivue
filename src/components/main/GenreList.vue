e7544729aadcf958c4f77a658fc8a1b41ea7de80
Artem Kolesnikov <katanaosu@vk.com>
2023-08-14 22:01:45 +0300

    ✨ Add GenreList.vue with new icons!

--- src/components/main/GenreList.vue
+++ src/components/main/GenreList.vue
@@ -1,22 +1,62 @@
 <script setup lang="ts">
+import AButton from '~/components/button/AButton.vue';
+import ACyberpunkIcon from '~/components/icons/ACyberpunkIcon.vue';
+import ADramaIcon from '~/components/icons/ADramaIcon.vue';
+import AFantasyIcon from '~/components/icons/AFantasyIcon.vue';
+import AFemaleIcon from '~/components/icons/AFemaleIcon.vue';
+import AMagicIcon from '~/components/icons/AMagicIcon.vue';
+import AMaleIcon from '~/components/icons/AMaleIcon.vue';
+import APsychologyIcon from '~/components/icons/APsychologyIcon.vue';
+import ARomanceIcon from '~/components/icons/ARomanceIcon.vue';
+import ASportIcon from '~/components/icons/ASportIcon.vue';
+import ATragedyIcon from '~/components/icons/ATragedyIcon.vue';
 
+const genreList = [
+  { text: 'Спорт', leftIcon: markRaw(ASportIcon) },
+  { text: 'Драма', leftIcon: markRaw(ADramaIcon) },
+  { text: 'Сёнен', leftIcon: markRaw(AMaleIcon) },
+  { text: 'Романтика', leftIcon: markRaw(ARomanceIcon) },
+  { text: 'Фэнтези', leftIcon: markRaw(AFantasyIcon) },
+  { text: 'Сёдзё', leftIcon: markRaw(AFemaleIcon) },
+  { text: 'Психология', leftIcon: markRaw(APsychologyIcon) },
+  { text: 'Киберпанк', leftIcon: markRaw(ACyberpunkIcon) },
+  { text: 'Магия', leftIcon: markRaw(AMagicIcon) },
+  { text: 'Трагедия', leftIcon: markRaw(ATragedyIcon) },
+];
 </script>
 
 <template>
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
+  <ul :class="$style.genreList">
+    <li
+      v-for="genre in genreList"
+      :key="genre.text"
+    >
+      <a-button
+        type="secondary"
+        size="xs"
+        :text="genre.text"
+      >
+        <template #left-icon>
+          <component
+            :is="genre.leftIcon"
+            :class="$style.genreLeftIcon"
+          />
+        </template>
+      </a-button>
+    </li>
   </ul>
 </template>
 
-<style module>
+<style module lang="scss">
+.genreList {
+  display: flex;
+  gap: 14px;
 
+  overflow-x: scroll;
+}
+
+.genreLeftIcon {
+  width: 24px;
+  height: 24px;
+}
 </style>
